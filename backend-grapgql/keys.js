dd0d80a71e6367ea2093901a2d39314a1bad5fb8
Taner Özer 😌 <tanerozer16@gmail.com>
2025-03-04 16:34:46 +0300

    keys js added

---
+++ backend-grapgql/keys.js
@@ -0,0 +1,7 @@
+// keys.js
+module.exports = {
+  PORT: process.env.PORT || 3033,
+  MONGODB_LOCAL_URI: process.env.MONGODB_LOCAL_URI || "your_local_mongodb_uri",
+  MONGODB_ATLAS_URI: process.env.MONGODB_ATLAS_URI || "your_atlas_mongodb_uri",
+  TOKEN_SECRET_KEY: "gizlianahtar1675s2ads65a5das",
+};
